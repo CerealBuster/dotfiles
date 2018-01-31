7d7af80878abdf9c418442753972fee7bef7a0ae
cerealbuster <raffyou@gmail.com>
2018-01-31 08:23:49 +0100

    cider configs

--- init.el
+++ init.el
@@ -22,6 +22,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9b1c580339183a8661a84f5864a6c363260c80136bd20ac9f00d7e1d662e936a" default)))
  '(delete-selection-mode nil)
  '(package-selected-packages
    (quote
